5c0c6066bade5b63e1c96e4627dfb35305272e66
Denise Bradley <porenn4u@outlook.com>
2016-05-31 21:04:57 -0400

    S3A1 Settings Service

---
+++ S3A1SettingsServiceDemoApp/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using S3A1SettingsServiceDemoApp.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace S3A1SettingsServiceDemoApp.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ S3A1SettingsServiceDemoApp/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using S3A1SettingsServiceDemoApp.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace S3A1SettingsServiceDemoApp.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
